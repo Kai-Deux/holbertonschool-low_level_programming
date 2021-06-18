77f67cc7ae3bb02db2cf421e7e40153ed8a77d8f
Alejandra Hincapie <alejandra612@hotmail.com>
2021-06-17 19:56:27 -0500

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * Function that prints a message
  */
 int main(void)
-    {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
