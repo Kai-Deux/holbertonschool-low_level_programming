7eaa565bb76874e7c9b8dc770c2544d7da7cb28d
Alejandra Hincapie <alejandra612@hotmail.com>
2021-06-18 16:37:18 -0500

    primer punto

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
@@ -15,7 +16,7 @@ int main(void)
     else if ( n < 0) 
         printf("%d is negative", n);
     else 
-        printf("$d is zero", n);
+        printf("%d is zero", n);
     
 	return (0);
 }
