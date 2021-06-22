d53bcebb0db2568eb56e4e93899de643ba17ac8c
Alejandra Hincapie <alejandra612@hotmail.com>
2021-06-22 10:59:59 -0500

    0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -4,8 +4,9 @@
 *Return: 0
 */
 int main(void) {
-char holberton[9] = {'h', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-for(int msg = 0;  msg <= 8; msg ++)
-    _putchar(holberton[msg]);
+char holberton[] = "Holberton";
+for (int msg = 0; msg <= 8; msg++)
+_putchar(holberton[msg]);
+_putchar('\n');
 return (0);
 }
