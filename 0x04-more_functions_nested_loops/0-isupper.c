567aa3a1e7b5b944a6a89aef73f482b90c18f851
Alejandra Hincapie <alejandra612@hotmail.com>
2021-06-25 12:56:25 -0500

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,14 +6,15 @@
   *
   * Return: 1 for upper letter or 0 for any else
   */
+
 int _isupper(int c)
 {
-if ((c>= 65) && (c <= 90))
-{
-return (1);
-}
-else
-{
-return (0);
-}
+        if ((c>= 65) && (c <= 90))
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
 }
