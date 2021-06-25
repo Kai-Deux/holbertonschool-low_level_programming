9bc6483b961f573de72d7e9430af258e9f1db9a4
Alejandra Hincapie <alejandra612@hotmail.com>
2021-06-25 15:37:00 -0500

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
 
 /**
-  * print_numbers -  multiplies two integers.
-  *
-  * Return: 0
-  */
+ * print_numbers - prints numbers from 0 to 9
+ * void: No Input Parameter
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 void print_numbers(void)
 {
