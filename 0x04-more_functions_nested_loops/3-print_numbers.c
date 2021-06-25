976b895dde9fad50fcee90821f311bc396a165c4
Alejandra Hincapie <alejandra612@hotmail.com>
2021-06-25 15:29:28 -0500

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+  * print_numbers -  multiplies two integers.
+  *
+  * Return: 0
+  */
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+
+		_putchar(number);
+
+	}
+	_pulchar('\n');
+}
