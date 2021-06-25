376b2a93505c26690dc27951adbc0ff48c377908
Alejandra Hincapie <alejandra612@hotmail.com>
2021-06-25 18:52:03 -0500

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,12 @@ void print_most_numbers(void)
 {
 	int number;
 
-	if (!(number == 50 && number == 52))
+	for (number = 48; number <= 57; number++)
 	{
-		for (number = 48; number <= 57; number++)
+		if (!(number == 50 && number == 52))
 		{
 
-		_putchar(number);
+			_putchar(number);
 
 		}
 	}
