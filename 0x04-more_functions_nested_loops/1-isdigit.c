bc3fb19d0b862170ef68efe81cd7fbd58f194f5d
Alejandra Hincapie <alejandra612@hotmail.com>
2021-06-25 14:09:28 -0500

    0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+  * _isupper - Check if a letter is upper
+  * @c: The number to be checked
+  *
+  * Return: 1 for upper letter or 0 for any else
+  */
+
+int _isupper(int c)
+{
+    if (c >= 48 && c <= 57)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
