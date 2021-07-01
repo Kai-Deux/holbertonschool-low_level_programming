d89502009062d2431b0730a2db3d0222fa5a0b79
Alejandra Hincapie <alejandra612@hotmail.com>
2021-06-30 19:53:05 -0500

    101 array

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
+/**
+ * main - random  numbers
+ *
+ * Return: 0 Always
+*/
 
 int main(void)
 {
-    int random;
-    int count;
-    int numbers;
+	int i;
+	int j = 0;
+	int randnum;
+	char p[100];
 
-    srand(time(NULL));
-    for (count = 0, numbers = 2772; numbers > 122; count++)
-    {
-        random = (rand() % 125) + 1;
-        printf("%c", random);
-        numbers -= random;
-    }
-    printf("%c", numbers);
-
-    return (0);
+	srand(time(NULL));
+	for (i = 0; i < 100; i++)
+	{
+		randnum = random() % 127;
+		if (randnum > 32)
+		{
+			p[j] = randnum;
+			j++;
+		}
+	}
+	p[j] = '\0';
+	printf("%s", p);
+	return (0);
 }
