1e05686ebbfa279451821c6c76a2be2194a81850
Alejandra Hincapie <alejandra612@hotmail.com>
2021-07-19 22:28:22 -0500

    1

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,21 +1,3 @@
-#include "1-pi.h"
-#include "1-pi.h"
-#include <stdio.h>
-
-/**
- * main - Create a header file that defines a macro named PI as an
- * abbreviation for the token 3.14159265359.
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	float a;
-	float r;
-
-	r = 98;
-	a = PI * r * r;
-	printf("%.3f\n", a);
-	return (0);
-}
+#ifndef PI
+#define PI 3.14159265359
+#endif
