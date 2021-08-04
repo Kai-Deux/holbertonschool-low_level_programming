0b3c21ebb9cef75d7af91946d6e8f304a0a66470
Alejandra Hincapie <alejandra612@hotmail.com>
2021-08-03 23:10:55 -0500

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,21 @@
 #include "lists.h"
-
 /**
- *listint_len - prints all the elements of a listint_t list.
- *@h: Pointer to list
- *Return: count.
+ * listint_len -  this function prints all the elements of a listint_t list.
+ * @h: linked list pointer.
+ * Return: Always 0.
  */
-
 size_t listint_len(const listint_t *h)
 {
-	size_t count;
+	size_t counter;
 
 	if (h == NULL)
-		return;
-	while (h != NULL)
+	{
+		return (0);
+	}
+
+	for (counter = 0; h != NULL; counter++)
 	{
 		h = h->next;
-		count++;
 	}
-	return (count);
+	return (counter);
 }
