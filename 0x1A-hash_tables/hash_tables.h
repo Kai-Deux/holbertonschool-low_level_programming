e40dbc39aa5b5a2a1adcb312b906b8904df4dab4
lahincapie <alejandra612@hotmail.com>
2021-10-10 22:40:31 -0500

    hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -7,7 +7,6 @@
 
 /**
  * struct hash_node_s - Node of a hash table
- *
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
@@ -22,7 +21,6 @@ typedef struct hash_node_s
 
 /**
  * struct hash_table_s - Hash table data structure
- *
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
@@ -34,34 +32,12 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-/**
- * 0. >>> ht = {}
-*/
 hash_table_t *hash_table_create(unsigned long int size);
-/**
- * 1. djb2
-*/
 unsigned long int hash_djb2(const unsigned char *str);
-/**
- * 2. key -> index
-*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-/**
- * 3. >>> ht['betty'] = 'cool'
-*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-/**
- * 4. >>> ht['betty']
-*/
 char *hash_table_get(const hash_table_t *ht, const char *key);
-/**
- * 5. >>> print(ht)
-*/
 void hash_table_print(const hash_table_t *ht);
-/**
- * 6. >>> del ht
-*/
 void hash_table_delete(hash_table_t *ht);
-
 
 #endif
